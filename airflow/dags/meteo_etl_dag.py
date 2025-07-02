51c0b9b66a5a310055b5072b951ef3139548f766
TsioryR98 <hei.tsiory.2@gmail.com>
2025-07-02 23:27:43 +0300

    feat; use @task for scripts and implements fetch_meteo_data only

---
+++ airflow/dags/meteo_etl_dag.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+import sys
+sys.path.insert(0, '/home/tsioryr/HEI-Etudes/data-airflow/airflow')
+from plugins.scripts.meteo_etl_process import fetch_meteo_data
+from airflow.decorators import task
+
+default_args = {
+    'start_date': datetime(2024, 1, 1),
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='meteo_etl_dag',
+    description='ETL 6:00 am',
+    schedule='*/5 * * * *',
+    default_args=default_args,
+    catchup=False,
+    tags=['etl', 'meteo']
+) as dag :
+    """FETCH METEO DATA FROM API"""
+    fetch_task = task(fetch_meteo_data)()
---
+++ airflow/plugins/scripts/meteo_etl_process.py
@@ -0,0 +1,54 @@
+
+import pandas as pd
+import requests
+from datetime import datetime
+from airflow.models import Variable
+from airflow.decorators import task
+import sys
+sys.path.insert(0, '/home/tsioryr/HEI-Etudes/data-airflow/airflow')
+from plugins.scripts.town_mapping import town_mapping
+
+
+@task
+def fetch_meteo_data():
+    """
+    EXTRACT DATA FROM API OPENWEATHERMAP
+    """
+    API_KEY = Variable.get("API_KEY")
+    records_data = []    #store data in a list
+    for location_id, town in town_mapping.items():
+        url = (
+            "https://api.openweathermap.org/data/2.5/weather?"
+            f"q={town}&units=metric&appid={API_KEY}"
+        )
+
+        data = requests.get(url, timeout=10).json()
+        records_data.append({
+            "location_id": location_id,
+            "ville": data["name"],
+            "temperature": data["main"]["temp"],
+            "humidity": data["main"]["humidity"],
+            "pression": data["main"]["pressure"],
+            "weather": data["weather"][0]["main"],
+            "temperature_max": data["main"]["temp_max"],
+            "temperature_min": data["main"]["temp_min"],
+            "temperature_feel": data["main"]["feels_like"],
+            "wind_speed": data["wind"]["speed"],
+            "rain_day": data.get("rain", {}).get("1h", 0),
+            "cloud_rate": data["clouds"]["all"],
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })
+
+    """
+    load data to CSV file and postgres
+    """
+    df = pd.DataFrame(records_data)
+    current_date = datetime.now().strftime("%Y-%m-%d_%H:%M")
+    filename = f"/home/tsioryr/HEI-Etudes/data-airflow/airflow/airflow-export/weather_{current_date}.csv"
+
+    "for docker"
+    #filename = f"/opt/airflow/airflow-export/weather_{current_date}.csv"
+    df.to_csv(filename, index=False)
+    return filename
+
+    
