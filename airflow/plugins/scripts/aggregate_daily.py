d88bdf418ba6f9f3e30b478ac9b7e206b594753c
TsioryR98 <hei.tsiory.2@gmail.com>
2025-07-05 21:29:51 +0300

    feat: add aggregate values with ordered column

--- airflow/plugins/scripts/aggregate_daily.py
+++ airflow/plugins/scripts/aggregate_daily.py
@@ -17,7 +17,15 @@ def aggregate_daily(combined_csv: str) -> str:
 
     df_daily = df.groupby(["location_id", "city", "date"])[agg_columns].mean().reset_index()
 
-    df_daily[agg_columns] = df_daily[agg_columns].round(3)  # 3 decimal places
+    df_daily[agg_columns] = df_daily[agg_columns].round(3)     # 3 decimal places
+
+    #ordered columns
+    ordered_cols = [
+        "location_id", "city", "apparent_temperature", "cloud_cover", "precipitation",
+        "rain", "snow", "soil_temperature", "temperature", "wind_speed", "time"
+    ]
+    ordered_cols = [col for col in ordered_cols if col in df_daily.columns]
+    df_daily = df_daily[ordered_cols]
 
     current_date = datetime.now().strftime("%Y-%m-%d_%H:%M")
     output_file = f"/home/tsioryr/HEI-Etudes/data-airflow/airflow/data/aggregated_daily_{current_date}.csv"
